901c833e464a01bc2339f3d3c277e148f6030117
Christopher Hunn <christopher.hunn@gmail.com>
2025-05-20 21:38:40 +0100

    fix: Workshop week4-05 now working

--- Workshops/Week04/05-Databases-Schema-Tables-and-SQL/server/server.js
+++ Workshops/Week04/05-Databases-Schema-Tables-and-SQL/server/server.js
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 import pg from "pg";
 
 const app = express();
+
+// IF YOU LEAVE OUT THE () on .json() you will not get and error, the get will not be returned
 app.use(express.json());
 dotenv.config();
 
+// IF YOU HAVE A CAPITOL C for connectionString you will get
+// Error: connect ECONNREFUSED 127.0.0.1:5432
 const db = new pg.Pool({
-  ConnectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 app.listen(8080, function () {
@@ -18,21 +22,20 @@ app.get("/", function (request, response) {
   response.json({ message: "Welcome to my server" });
 });
 
-// app.get("/messages", async function (request, response) {
-//   const q = await db.query(`SELECT * FROM messages;`);
-
-//   //  const data = response.json(messages.rows);
-//   //  console.log("data:", data);
-
-//   // this below works with the above commented out
-//   // proving that the route works and is being called
-//   response.json({ message: "what messages" });
-// });
-
-// Create a new database for storing some data sent to your API
-// completed as part of the example given in the workshop
+// This was copied and pasted directly from Manny's demo repo
+// I used the queries.sql to build the staff table on supabase.com
+// once this failed the same as mine I searched further and found the capitol C noted above.
+app.get("/staff", async function (request, response) {
+  //read the staff data in our database
+  const query = await db.query(`SELECT * FROM staff;`);
+  //   console.log(query); //this is a Response object
+  //parse the data into JSON and wrangle the data to just be the rows property
+  const data = response.json(query.rows);
+  //   console.log(data); //this is our wrangled data in JSON format
+});
 
 app.get("/messages", async function (request, response) {
   const messages = await db.query("SELECT * FROM messages");
-  response.json(messages.rows);
+  const data = response.json(messages.rows);
+  console.log(data);
 });
